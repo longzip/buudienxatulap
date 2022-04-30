529aa02524c48dd271c7d47a5eb476e075c8ad75
Lo Van Long <longlv@v-auto.com.vn>
2022-04-30 16:15:37 +0700

    generate

--- docs/static/1651309948/tinh-muc-dong-va-luong-huu-du-tinh/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Fwww.buudienxatulap.ga\u002Fstatic\u002F1651309948",layout:"default",error:null,serverRendered:true,routePath:"\u002Ftinh-muc-dong-va-luong-huu-du-tinh",config:{_app:{basePath:a,assetsPath:a,cdnURL:"https:\u002F\u002Fwww.buudienxatulap.ga\u002F"}}}}("\u002F"));
---
+++ docs/static/1651310136/mua-bao-hiem-y-te-o-buu-dien/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Fwww.buudienxatulap.ga\u002Fstatic\u002F1651310136",layout:"default",error:null,serverRendered:true,routePath:"\u002Fmua-bao-hiem-y-te-o-buu-dien",config:{_app:{basePath:a,assetsPath:a,cdnURL:"https:\u002F\u002Fwww.buudienxatulap.ga\u002F"}}}}("\u002F"));
